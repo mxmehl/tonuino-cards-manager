6a20e0631336670829fff92d33661186e3f27cc5
Max Mehl <mail@mehl.mx>
2024-06-14 13:15:35 +0200

    initial commit, taking over code from provate repository

---
+++ tonuino_cards_manager/__init__.py
@@ -0,0 +1,13 @@
+"""Global constants"""
+
+MODES = {
+    "play-random": 1,
+    "album": 2,
+    "party": 3,
+    "single": 4,
+    "audiobook": 5,
+    "admin": 6,
+    "play-from-to": 7,
+    "album-from-to": 8,
+    "party-from-to": 9,
+}
