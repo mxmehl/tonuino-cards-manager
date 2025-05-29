46999c807405e0b6f51ec456375f93452038a9ba
Sonnen2blume <sonnen2blume@proton.me>
2025-05-29 16:42:34 +0200

    add feature to toggle naming of copied files

--- tonuino_cards_manager/_helpers.py
+++ tonuino_cards_manager/_helpers.py
@@ -7,6 +7,7 @@
 import logging
 import re
 import shutil
+import sys
 from pathlib import Path
 
 from mutagen.easyid3 import EasyID3
@@ -18,24 +19,38 @@ def _sanitize_filename(filename: str) -> str:
     return re.sub("[^A-Za-zÄÖÜäöü0-9-_]+", "", filename.replace(" ", "_"))
 
 
-def copy_to_sdcard(index: int, mp3file: Path, destination_dir: Path) -> None:
+def copy_to_sdcard(index: int, mp3file: Path, destination_dir: Path, filenametype: str) -> None:
     """Copy a single file to the SD card in a suitable"""
     logging.debug("Processing %s", mp3file)
-    # If no ID3 tags are present, use file name, otherwise $artist-$title
-    try:
-        meta = EasyID3(mp3file)
-        filename = _sanitize_filename(f"{meta['artist'][0]}-{meta['title'][0]}")
-    except (ID3NoHeaderError, KeyError):
-        logging.debug("File %s does not contain any ID3 tags. Using its file name", mp3file.name)
-        filename = _sanitize_filename(mp3file.stem)
+    if filenametype=="mp3tags":
+        # If no ID3 tags are present, use file name, otherwise $artist-$title
+        try:
+            meta = EasyID3(mp3file)
+            filename = _sanitize_filename(f"{meta['artist'][0]}-{meta['title'][0]}")
+        except (ID3NoHeaderError, KeyError):
+            logging.debug("File %s does not contain any ID3 tags."
+                "Using its file name", mp3file.name
+            )
+            filename = _sanitize_filename(mp3file.stem)
 
-    # copy file to destination using a compatible name based on tags
-    destname = "{track}-{filename}.mp3".format(  # pylint: disable=consider-using-f-string
-        # Track number, filled with leading zeros
-        track=str(index + 1).zfill(3),
-        # Artist and title / or filename
-        filename=filename,
-    )
+        # copy file to destination using a compatible name based on tags
+        destname = "{track}-{filename}.mp3".format(  # pylint: disable=consider-using-f-string
+            # Track number, filled with leading zeros
+            track=str(index + 1).zfill(3),
+            # Artist and title / or filename
+            filename=filename,
+        )
+    elif filenametype=="tracknumber":
+        destname = "{track}.mp3".format(  # pylint: disable=consider-using-f-string
+            # Track number, filled with leading zeros
+            track=str(index + 1).zfill(3)
+        )
+    else:
+        logging.critical("You did specify a wrong filenametype '%s'."
+            "Supported are: 'mp3tags' and 'tracknumber'. ", filenametype
+        )
+        sys.exit(1)
+
     logging.debug("Copying %s to %s", mp3file, destination_dir / Path(destname))
     shutil.copy2(mp3file, destination_dir / Path(destname))
 
